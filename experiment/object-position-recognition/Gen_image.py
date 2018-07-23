b1ce3298fcafc2394e96042348af2429ade2e31b
Ekkasit Pingpong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2018-07-23 12:52:02 -0400

    separate train-test-val

--- experiment/object-position-recognition/Gen_image.py
+++ experiment/object-position-recognition/Gen_image.py
@@ -9,16 +9,16 @@ class Gen_image:
     def __init__(self, size=(800, 800)):
         self.size = size
 
-    def circles(self, num_image):
+    def circles(self, num_image, path="data/circles"):
         def draw_circle(draw, x, y, r):
             draw.ellipse((x-r, y-r, x+r, y+r), fill=self._get_random_color())
-        return self._base_geo(num_image, path="data/circles", geo_function=draw_circle)
+        return self._base_geo(num_image, path=path, geo_function=draw_circle)
 
-    def squares(self, num_image):
+    def squares(self, num_image, path="data/squares"):
         def draw_square(draw, x, y, r):
             draw.rectangle(((x-r, y-r), (x+r, y+r)),
                            fill=self._get_random_color())
-        return self._base_geo(num_image, path="data/squares", geo_function=draw_square)
+        return self._base_geo(num_image, path=path, geo_function=draw_square)
 
     def _base_geo(self, num_image, path, geo_function):
         locations = []
@@ -27,7 +27,7 @@ class Gen_image:
         for id in range(num_image):
             x = randint(low, high) # randint(0, self.size[0])
             y = randint(low, high) # randint(0, self.size[1])
-            r = 4 #randint(ceil(self.size[0]/10), self.size[0]/2)
+            r = randint(ceil(self.size[0]/10), self.size[0]/4)
 
             img = Image.new('RGB', self.size, (255, 255, 255))
             draw = ImageDraw.Draw(img)
@@ -42,10 +42,16 @@ class Gen_image:
         return (randint(0, 255), randint(0, 255), randint(0, 255), 255)
 
 
-gen_image = Gen_image((28, 28))
-circle_locations = gen_image.circles(512)
-square_locations = gen_image.squares(512)
+gen_image = Gen_image((128, 128))
+circle_locations_train = gen_image.circles(64, "data/circles_train")
+save_csv(circle_locations_train, './data/circles_train/locations.csv')
 
-save_csv(circle_locations, './data/circles/locations.csv')
-save_csv(square_locations, './data/squares/locations.csv')
+circle_locations_test = gen_image.circles(64, "data/circles_test")
+save_csv(circle_locations_test, './data/circles_test/locations.csv')
+
+circle_locations_val = gen_image.circles(64, "data/circles_val")
+save_csv(circle_locations_val, './data/circles_val/locations.csv')
+
+# square_locations = gen_image.squares(64, "data/squares")
+# save_csv(square_locations, './data/squares/locations.csv')
 
