b9d47a9eb3db648d4b8a9cac5ffbcd7bbc4ba3be
Ekkasit Pingpong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2018-12-29 17:12:35 -0500

    add semi-hard

--- EXITlib/exitai/exitai/learner.py
+++ EXITlib/exitai/exitai/learner.py
@@ -6,16 +6,14 @@ from .helpers import get_param_groups_with_lr, get_callbacks_by_tuple_string
 
 
 class Learner:
-    def __init__(self, data_loader_train, data_loader_test, model, criterion, input_to_model_mapper):
+    def __init__(self, data_loader_train, data_loader_test, model):
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
         self.data_loader_train = data_loader_train
         self.data_loader_test = data_loader_test
         self.model = model.to(self.device)
-        self.criterion = criterion
-        self.input_to_model_mapper = input_to_model_mapper
 
-    def _run_model(self, phase, optimizer, callback: Callable=Callback()):
+    def _run_model(self, phase, optimizer, model_loss_func, callback: Callable=Callback()):
 
         callback.on_epoch_begin()
         if phase == 'train':
@@ -30,13 +28,9 @@ class Learner:
                 callback.on_batch_begin()
                 input_data = tuple(i.to(self.device) for i in data)
 
-                input_model = self.input_to_model_mapper(*input_data)
-                if type(input_model) is tuple:
-                    output = self.model(*input_model)
-                else:
-                    output = self.model(input_model)
+                output, loss, *is_skip = model_loss_func(input_data, self.model, phase)
 
-                loss = self.criterion(input_data, output)
+                if len(is_skip)>0: continue
 
                 if phase == 'train':
                     optimizer.zero_grad()
@@ -49,7 +43,13 @@ class Learner:
 
         callback.on_epoch_end(phase, len(data_loader.dataset))
 
-    def fit(self, lr: float=0.003, num_epochs: int=25, cycle: dict = {'cycle_len': 2, 'cycle_mult': 2}, callbacks: any=None, eval_func=classification_eval_func, early_stop='acc')->None:
+    def fit(self, model_loss_func,
+                  lr: float=0.003, 
+                  num_epochs: int=25, 
+                  cycle: dict = {'cycle_len': 2, 'cycle_mult': 2}, 
+                  callbacks: any=None, 
+                  eval_func=classification_eval_func, 
+                  early_stop='acc')->None:
         model_params = get_param_groups_with_lr(self.model, lr)
         optimizer = torch.optim.Adam(model_params)
 #         optimizer = torch.optim.SGD(model_params, momentum=0.9, weight_decay=5e-4)
@@ -80,22 +80,22 @@ class Learner:
         test_callback.on_train_begin()
         for i in range(num_epochs):
             print(f'---- epoch:{i} ------')
-            self._run_model('train', optimizer, train_callback)
-            self._run_model('test', optimizer, test_callback)
+            self._run_model('train', optimizer, model_loss_func, train_callback)
+            self._run_model('test', optimizer, model_loss_func, test_callback)
             if test_callback.is_done():
                 break
         train_callback.on_train_end()
         test_callback.on_train_end()
 
-    def predict(self, eval_func=classification_eval_func):
-        self._run_model('test', None, FitTracker(eval_func))
+    def predict(self, model_loss_func, eval_func=classification_eval_func):
+        self._run_model('test', None, model_loss_func, FitTracker(eval_func))
 
-    def lr_find(self, start_lr: float=1e-7, end_lr: float=10, num_it: int=100, num_batch: int=10)->None:
+    def lr_find(self, model_loss_func, start_lr: float=1e-7, end_lr: float=10, num_it: int=100, num_batch: int=10)->None:
         optimizer = torch.optim.Adam(self.model.parameters(), lr=start_lr)
         lr_finder = LRFinder(start_lr, end_lr, num_it,
                              optimizer, num_batch=num_batch)
         while True:
-            self._run_model('train', optimizer, lr_finder)
+            self._run_model('train', optimizer, model_loss_func, lr_finder)
             if lr_finder.is_done():
                 break
         plt.semilogx(lr_finder.lrs, lr_finder.losses)
