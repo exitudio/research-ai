2a4c12527a108a0d5b5059cd2e3ea75aa6f8b551
EC2 Default User <ec2-user@ip-172-16-88-74.us-west-2.compute.internal>
2019-01-14 00:03:40 +0000

    run visualize aws

--- EXITlib/exitai/exitai/losses.py
+++ EXITlib/exitai/exitai/losses.py
@@ -2,20 +2,26 @@ import torch.nn as nn
 from torch.autograd.function import Function
 import torch
 
-class CenterLossTest(nn.Module):
+'''
+ - To compare the different Center Loss & autograd, set inital Param(center) to one rather than random
+ - The things that is different is the paper add "1" for each class of center grad
+'''
+class CenterLossAutograd(nn.Module):
     def __init__(self, num_class, vector_size):
-        super(CenterLossTest, self).__init__()
-        self.centers = nn.Parameter(torch.ones((num_class, vector_size)))
+        super(CenterLossAutograd, self).__init__()
+#         self.centers = nn.Parameter(torch.ones((num_class, vector_size)))
+        self.centers = nn.Parameter(torch.randn(num_class, vector_size))
     def forward(self, target, vector_embedding):
         center_by_target = self.centers.index_select(0, target.long())
         diff_vector_from_center = vector_embedding - center_by_target
-        return 1/2 * diff_vector_from_center.pow(2).sum()
+        batch_size = diff_vector_from_center.size(0)
+        return 1/2 * diff_vector_from_center.pow(2).sum() / batch_size
     
 class CenterLoss(nn.Module):
     def __init__(self, num_class, vector_size):
         super(CenterLoss, self).__init__()
-        # self.centers = nn.Parameter(torch.randn(num_class, vector_size))
-        self.centers = nn.Parameter(torch.ones((num_class, vector_size)))
+        self.centers = nn.Parameter(torch.randn(num_class, vector_size))
+#         self.centers = nn.Parameter(torch.ones((num_class, vector_size)))
         self.center_loss_function = CenterLossFunction.apply
 
     def forward(self, target, vector_embedding):
@@ -30,16 +36,16 @@ class CenterLossFunction(Function):
         diff_vector_from_center = vector_embedding - center_by_target
         ctx.save_for_backward(diff_vector_from_center, target, centers)
 
-        # check with notebook at home about i,j,yi
-        return 1/2 * diff_vector_from_center.pow(2).sum()
+        batch_size = diff_vector_from_center.size(0)
+        return 1/2 * diff_vector_from_center.pow(2).sum() / batch_size
 
     @staticmethod
     def backward(ctx, grad_output):
         diff_vector_from_center, target, centers = ctx.saved_tensors
-        batch_size = 1 # diff_vector_from_center.size(0)
+        batch_size = diff_vector_from_center.size(0)
 
         # maybe try to fill 0.00000001
-        counts = centers.new_full((centers.size(0), 1), 1e-8)
+        counts = centers.new_ones((centers.size(0), 1)) # centers.new_full((centers.size(0), 1), 1e-8)
         ones = centers.new_ones((target.size(0), 1))
 
         counts = counts.scatter_add_(0, target.unsqueeze(1).long(), ones)
@@ -48,16 +54,7 @@ class CenterLossFunction(Function):
             diff_vector_from_center.size()).long(), diff_vector_from_center)
         grad_centers = grad_centers/counts
 
-        # print('counts:', counts)
-        # print('target:', target)
-        # print('ones:', ones)
-        # print('diff_vector_from_center:', diff_vector_from_center)
-        # print('grad_centers:', grad_centers)
-        # print('counts:', counts)
-        # print(grad_centers/counts)
-
-        # why do we need to *diff/batchsize????
-        return None, grad_output * diff_vector_from_center / batch_size, -grad_centers/batch_size,
+        return None, grad_output * diff_vector_from_center / batch_size, -grad_centers / batch_size,
 
 
 def main():
@@ -69,18 +66,18 @@ def main():
                                      [12, 12],
                                      [13, 13]], dtype=torch.float).to(device).requires_grad_()
     ct_loss = CenterLoss(10, vector_embedding.shape[1]).to(device)
-    ct_loss_test = CenterLossTest(10, vector_embedding.shape[1]).to(device)
+    ct_loss_autograd = CenterLossAutograd(10, vector_embedding.shape[1]).to(device)
     # print (list(ct.parameters()))
     optimizer = torch.optim.SGD(ct_loss.parameters(), lr=0.5)
-    optimizer_test = torch.optim.SGD(ct_loss_test.parameters(), lr=0.5)
+    optimizer_test = torch.optim.SGD(ct_loss_autograd.parameters(), lr=0.5)
 
     for i in range(2):
         out = ct_loss(target, vector_embedding)
-        out_test = ct_loss_test(target, vector_embedding)
+        out_test = ct_loss_autograd(target, vector_embedding)
         out.backward()
         out_test.backward()
         print('ct_loss:', ct_loss.centers.grad)
-        print('ct_loss_test:', ct_loss_test.centers.grad)
+        print('ct_loss_autograd:', ct_loss_autograd.centers.grad)
         optimizer.step()
         optimizer.zero_grad()
         optimizer_test.step()
